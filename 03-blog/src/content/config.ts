0c73f2b9741b6179f3057db38ea7423d76926979
Javier Vinueza <javiervin@hotmail.com>
2024-09-17 23:55:52 -0500

    feat: Add Image component to TypedBlogPost.astro
    This commit adds the Image component from the astro:assets package to the TypedBlogPost.astro file. The Image component is used to display the frontmatter image of a blog post. It includes attributes such as width, height, quality, and loading. The commit also includes a console.log statement for debugging purposes.

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -2,17 +2,20 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
-    // Relacion
-    author: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "La imagen debe ser de al menos 1200px de ancho",
+      }),
+      // Relacion
+      author: z.string(),
 
-    // Relacion
-    tags: z.array(z.string()),
-  }),
+      // Relacion
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
