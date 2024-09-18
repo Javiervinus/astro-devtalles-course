63f879ec6bab888b007f9f7eee981ba60fe4cddf
Javier Vinueza <javiervin@hotmail.com>
2024-09-18 00:40:14 -0500

    feat: 04-blog with author relations

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -11,13 +11,24 @@ const blogCollection = defineCollection({
         message: "La imagen debe ser de al menos 1200px de ancho",
       }),
       // Relacion
-      author: z.string(),
+      // author: z.string(),
+      author: reference("author"),
 
       // Relacion
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
